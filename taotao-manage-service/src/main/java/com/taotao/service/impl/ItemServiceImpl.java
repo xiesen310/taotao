86ccb08fa9f2cae889a1a228241aacee9e0eaa3a
xiesen310 <xiesen310.163.com>
2017-09-15 22:22:17 +0800

    搭建环境测试完成

---
+++ taotao-manage-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -0,0 +1,44 @@
+package com.taotao.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
+import com.taotao.pojo.TbItemExample.Criteria;
+import com.taotao.service.ItemService;
+
+/**
+ * 商品管理
+* @ClassName: ItemServiceImpl
+* @Description: TODO
+* @author Allen
+* @date 2017年9月15日 下午9:39:30
+*
+ */
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+	@Autowired
+	private TbItemMapper itemMapper;
+	
+	@Override
+	public TbItem getItemById(long itemId) {
+		// TbItem item = itemMapper.selectByPrimaryKey(itemId);
+		
+		TbItemExample example = new TbItemExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andIdEqualTo(itemId);
+		List<TbItem> list = itemMapper.selectByExample(example);
+		if(list != null && list.size() > 0){
+			TbItem item = list.get(0);
+			return item;
+		}
+		return null;
+	}
+
+}
